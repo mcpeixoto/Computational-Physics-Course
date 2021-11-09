acbec24c2e9a703b2ea06d04f0401829d4f6c398
Miguel Caçador Peixoto <miguelpeixoto457@gmail.com>
2021-11-09 11:22:43 +0000

    improvements

--- test2.py
+++ test2.py
@@ -1,11 +1,4 @@
-# Calculate the density operator of a linear combination
-# of two density operators.
-# input:
-#   - n: number of sites
-#   - m: number of particles
-#   - k: number of particles in the first state
-#   - l: number of particles in the second state
-#   - p: probability of the first state
-# output:
-#   - density operator
 
+
+
+def newton
